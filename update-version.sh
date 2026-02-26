03be999ec838f4acce1c0e527b5a1e9f6bcea4ae
jyz0501 <jyz_0501@live.cn>
2026-02-26 21:00:37 +0800

    更新版本号生成逻辑和GitHub Actions配置

--- update-version.sh
+++ update-version.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
-# 获取 git 提交次数
+# 获取提交次数
 COMMIT_COUNT=$(git rev-list --count HEAD)
 
-# 生成版本号（基于提交次数）
-VERSION="v1.$COMMIT_COUNT.0"
+# 生成版本号
+VERSION="v1.${COMMIT_COUNT}.0"
 
-# 更新 index.html 中的版本号
-sed -i.bak "s|<span id=\"app-version\"[^>]*>.*</span>|<span id=\"app-version\" style=\"font-size: 12px; color: #999;\">$VERSION</span>|" index.html
+# 更新 index.html 文件中的版本号
+sed -i '' "s/<span id=\"app-version\" style=\"font-size: 12px; color: #999;\">.*<\/span>/<span id=\"app-version\" style=\"font-size: 12px; color: #999;\">${VERSION}<\/span>/" index.html
 
-# 删除备份文件
-rm -f index.html.bak
-
-echo "版本号已更新为: $VERSION"
+# 输出结果
+echo "版本号已更新为: ${VERSION}"
